975579c70df0395d900d38f15c399dff862f52e3
vhhss <monstersapbe@gmail.com>
2026-05-19 22:00:12 -0300

    feat: implementado formulario de registro con guardado en firestore

--- src/app/components/registro/registro.ts
+++ src/app/components/registro/registro.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './registro.html',
-  styleUrl: './registro.css',
+  styleUrls: ['./registro.css']
 })
-export class Registro {}
+export class Registro {
+  private authService = inject(AuthService);
+
+  correo: string = '';
+  contrasenia: string = '';
+  nombre: string = '';
+  apellido: string = '';
+  edad!: number;
+  mensajeError = signal<string | null>(null);
+
+  async onRegistro() {
+    this.mensajeError.set(null);
+    if (!this.correo || !this.contrasenia || !this.nombre || !this.apellido || !this.edad) {
+      this.mensajeError.set('Por favor, complete todos los campos del formulario.');
+      return;
+    }
+    try {
+      await this.authService.registrar(this.correo, this.contrasenia, this.nombre, this.apellido, this.edad);
+    } catch (err: any) {
+      this.mensajeError.set(err);
+    }
+  }
+}
